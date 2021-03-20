3d9d5df5311a955d456f678f6de222c10b22f6e0
Sahajpal <sahajpal80@gmail.com>
2021-03-21 02:41:27 +0530

    Intial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sahajpal Singh Mahla on 21/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
